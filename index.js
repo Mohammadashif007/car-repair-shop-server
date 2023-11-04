99decf0bb06e8169693840bb4ac87d923789c43c
Mohammad Ashif <mohammadashif510@gmail.com>
2023-11-04 14:40:12 +0600

    dynamic service page added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const app = express();
@@ -31,6 +31,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db('carShopDB').collection('services');
+
+    app.get('/services', async (req, res) => {
+        const cursor = servicesCollection.find();
+        const result = await cursor.toArray();
+        console.log(result)
+        res.send(result); 
+    })
+
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title: 1, price: 1, img: 1, service_id: 1 },
+          };
+        const result = await servicesCollection.findOne(query, options);
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
